25f1d79921c740ea4f748cce09c5b3ef8d445710
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-03 20:37:35 +0000

    initial commit & employee class

---
+++ src/main/java/Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+    private String name;
+    private String nationalInsurance;
+    private double salary;
+
+    public Employee(String name, String nationalInsurance, double salary) {
+        this.name = name;
+        this.nationalInsurance = nationalInsurance;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNationalInsurance() {
+        return this.nationalInsurance;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void salaryIncrease(double salaryIncrement) {
+        this.salary = this.salary * salaryIncrement;
+    }
+
+    public double payBonus() {
+        return this.salary * 0.01;
+    }
+}
